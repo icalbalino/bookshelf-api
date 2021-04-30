6c7dc490e41e992068feedeb2bcb05eadbc90f53
if0balino <balino.ical@gmail.com>
2021-05-01 03:20:26 +0700

    deleteBookByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -155,16 +155,7 @@ const editBookByIdHandler = (request, h) => {
 
   if (index !== -1) {
     books[index] = {
-      ...books[index],
-      name,
-      year,
-      author,
-      summary,
-      publisher,
-      pageCount,
-      readPage,
-      reading,
-      updatedAt,
+      ...books[index], name, year, author, summary, publisher, pageCount, readPage, reading, updatedAt,
     };
 
 
@@ -190,4 +181,32 @@ const editBookByIdHandler = (request, h) => {
 };
 
 
-module.exports = {addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler};
+const deleteBookByIdHandler = (request, h) => {
+  const {bookId} = request.params;
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index !== -1) {
+    books.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    });
+
+
+    response.code(200);
+    return response;
+  }
+
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan',
+  });
+
+
+  response.code(404);
+  return response;
+};
+
+
+module.exports = {addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler};
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 /* eslint-disable linebreak-style */
-const {addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler} = require('./handler');
+const {addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler} = require('./handler');
 
 const routes = [
   {
@@ -22,6 +22,11 @@ const routes = [
     method: 'PUT',
     path: '/books/{bookId}',
     handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
   },
 ];
 
